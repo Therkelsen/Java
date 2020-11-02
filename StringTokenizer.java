b1ba9f598673dd7bb1e6e451547fc2cd0ab30535
Therkelsen <37107686+Therkelsen@users.noreply.github.com>
2020-11-02 15:18:07 +0100

    String Tokenizer done

--- StringTokenizer.java
+++ StringTokenizer.java
@@ -4,38 +4,41 @@ public class StringTokenizer {
     String delim;
     int indeks;
 
-    public StringTokenizer(String source) {
+    public StringTokenizer() {
+        tekst = "N/A";
+        delim = "\n";
+        indeks = 0;
+    }
 
+    public StringTokenizer(String source) {
         tekst = source;
         delim = "\n";
         indeks = 0;
-
     }
 
     public StringTokenizer(String source, String delimiters) {
-
         tekst = source;
-        
         delim = delimiters;
-        
         indeks = 0;
+    }
 
+    public String getSource() {
+        return tekst;
     }
 
     public boolean isDelimiter(char tegn) {
         //System.out.println(delim.indexOf(tegn));
-        if (delim.indexOf(tegn) == -1)
-            return false;
-        else
-            return true;
+        for (int i = 0; i < delim.length(); i++) {
+            if (tegn == delim.charAt(i)) {
+                return true;
+            }
+        }
+        return false;
     }
 
-    public boolean hasMoreTokens(int index){
-        indeks = index;
-            if (indeks < (tekst.length()-1)) {
-                return true;
-            } else if (indeks == (tekst.length())) {
-                if (!isDelimiter(tekst.charAt(index))){
+        public boolean hasMoreTokens(){
+            for (int i = indeks; i < tekst.length(); i++) {
+                if (!isDelimiter(tekst.charAt(i))) {
                     return true;
                 }
             }
@@ -44,28 +47,29 @@ public class StringTokenizer {
 
     public String nextToken() {
         String token = "";
-        if (!hasMoreTokens(indeks)){
-            return "";
-        } else {
-            for (int i = indeks; i < tekst.length(); i++) {
-                if (!isDelimiter(tekst.charAt(i))){
-                    token += tekst.charAt(i);
-                }
-                else if (isDelimiter(tekst.charAt(i)) && token.length() > 0){
-                    indeks = i;
-                    break;
-                }
-            }
+        
+        while (indeks < tekst.length() && isDelimiter(tekst.charAt(indeks))) {
+            indeks++;
         }
+
+        while (indeks < tekst.length() && !isDelimiter(tekst.charAt(indeks))) {
+            token += tekst.charAt(indeks);
+            indeks ++;
+        }
+        
         return token;
     }
 
     public String nextToken(String delimiters) {
-        return "";
+        delim = delimiters;
+        return nextToken();
     }
 
     public int countTokens() {
-        return 0;
+        int counter = 0;
+        while (nextToken() != "") {
+            counter++;
+        }
+        return counter;
     }
-
 }
--- StringTokenizerMain.java
+++ StringTokenizerMain.java
@@ -2,17 +2,27 @@
 public class StringTokenizerMain {
     public static void main(String[] args) {
 
-        String source = "abc def ghi";
+        String source = "This is not ok.";
         String delims = " ,.";
 
         StringTokenizer str = new StringTokenizer(source, delims);
-        System.out.println("Source length: " + source.length());
-        
+        System.out.println("Source \"" + str.getSource() + "\" Has length: " + source.length() + " characters");
+        System.out.println("Analyzing source");
+        System.out.println();
+
         for (int i = 0; i < source.length(); i++) {
-            System.out.println("Char at " + (i+1) + " is delimiter: " + str.isDelimiter(source.charAt(i)));
-            System.out.println("Source at " + (i+1) + " has more tokens: " + str.hasMoreTokens(i));
-            System.out.println("Next token at " + (i+1) + ": " + str.nextToken());
-            System.out.println("");
+            if(!str.hasMoreTokens()) {
+            System.out.println("No more characters in source");
+            str.indeks = 0;
+            break;
+            } else {
+                System.out.println("Char at " + (i+1) + " is delimiter: " + str.isDelimiter(source.charAt(i)));
+                System.out.println("Source at " + (i+1) + " has more tokens: " + str.hasMoreTokens());
+                System.out.println("Next token at " + (i+1) + ": " + str.nextToken());
+                System.out.println("");
+            }
+
         }
+        System.out.println("Total tokens: " + str.countTokens());
     }
 }
