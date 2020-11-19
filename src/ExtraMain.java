08288c333f3092548a305b08e615146d9e6202aa
Therkelsen <37107686+Therkelsen@users.noreply.github.com>
2020-11-19 14:43:00 +0100

    .

--- src/ExtraMain.java
+++ src/ExtraMain.java
@@ -1,6 +1,10 @@
 
+package javalearning.src;
 
+import ExtraAssignments.java;
 
 public class ExtraMain {
-    
+    public static void main(String args[]) {
+        javalearning.src.ExtraAssignments.Assignment1();
+    }
 }
